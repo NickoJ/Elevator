cf2f98f9bc832bacbabbdf45cf3e4cbdd861a81c
Nickolay Klyukay <klyukay@gmail.com>
2019-04-13 19:44:16 +0300

    Add lift opening/closing inside indicator

---
+++ Assets/Scripts/Views/Lift/LiftColorIndicator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using Klyukay.Lift.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Klyukay.Lift.Views.Lift
+{
+
+    public class LiftColorIndicator : LiftView
+    {
+
+        [SerializeField] private float switchTime = 0.2f;
+        [SerializeField] private Graphic[] graphics;
+        
+        //TODO: To scriptable object
+        [SerializeField] private Color activeColor;
+        [SerializeField] private Color openedColor;
+        [SerializeField] private Color switchColor;
+
+        protected override void SetupConnections(ILift lift)
+        {
+            base.SetupConnections(lift);
+            lift.OnStateChanged += LiftStateChanged;
+        }
+
+        protected override void DeleteConnections(ILift lift)
+        {
+            base.DeleteConnections(lift);
+            lift.OnStateChanged -= LiftStateChanged;
+        }
+
+        protected override void UpdateView() => LiftStateChanged(Lift.State);
+
+        private void LiftStateChanged(LiftState inState)
+        {
+            StopAllCoroutines();
+
+            switch (inState)
+            {
+                case LiftState.Moving: 
+                case LiftState.Closed:
+                    LaunchActiveState(); 
+                    break;
+                case LiftState.Opening:
+                case LiftState.Closing:
+                    LaunchSwitchingState();
+                    break;
+                case LiftState.Opened:
+                    LaunchOpenedState();
+                    break;
+            }
+        }
+
+        private void LaunchActiveState() => SetColor(activeColor);
+        private void LaunchSwitchingState() => StartCoroutine(SwitchingRoutine());
+        private void LaunchOpenedState() => SetColor(openedColor);
+        
+        private IEnumerator SwitchingRoutine()
+        {
+            var switchWait = new WaitForSeconds(switchTime);
+            var nextColor = switchColor;
+            while (true)
+            {
+                SetColor(nextColor);
+                nextColor = nextColor == switchColor ? activeColor : switchColor;
+                yield return switchWait;
+            }
+        }
+        
+        private void SetColor(Color color)
+        {
+            foreach (var g in graphics) g.color = color;
+        }
+        
+    }
+
+}
